e8eb3b575a09489b36afcedb29949dd36fdde233
dev68 <33572559+dev68@users.noreply.github.com>
2017-11-18 23:43:49 +0530

    Update and rename omni_d855.mk to omni_g3ds.mk

--- omni_g3ds.mk
+++ omni_g3ds.mk
@@ -1,6 +1,6 @@
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/omni/config/common.mk)
 
-$(call inherit-product, device/lge/d855/full_d855.mk)
+$(call inherit-product, device/lge/g3ds/full_g3ds.mk)
 
-PRODUCT_NAME := omni_d855
+PRODUCT_NAME := omni_g3ds
